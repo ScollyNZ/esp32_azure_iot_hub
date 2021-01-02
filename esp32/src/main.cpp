6b468ea4193b4bdb901c2962657aab9ba0769e95
Simon Scoltock <simon@scoltock.co.nz>
2021-01-02 18:01:02 +1300

    initial set up

---
+++ esp32/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "passwords.cpp"
+
+void setup() {
+  // connect to WiFi
+  // Try home first, then try mobile
+  
+  sprintf('%s',houseWiFiPassword);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
